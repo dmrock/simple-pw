849e867f30db097561422c79feeaffe4d747331b
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-07-27 09:18:51 +0200

    chore: add ESLint and Prettier configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  ignorePatterns: ["dist/", "node_modules/"],
+};
